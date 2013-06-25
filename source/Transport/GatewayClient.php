e9e147d812f8d6dd8f276eaf460f560ed41a3449
imenem <imenem@inbox.ru>
2013-06-25 13:51:27 +0300

    Refactored GatewayClient.

--- source/Transport/GatewayClient.php
+++ source/Transport/GatewayClient.php
@@ -1,8 +1,10 @@
 <?php
 namespace PaynetEasy\Paynet\Transport;
 
-use PaynetEasy\Paynet\Exception\RequestException;
 use PaynetEasy\Paynet\Transport\Response;
+
+use PaynetEasy\Paynet\Exception\RequestException;
+use PaynetEasy\Paynet\Exception\ResponseException;
 use Exception;
 
 class GatewayClient implements GatewayClientInterface
@@ -15,20 +17,72 @@ class GatewayClient implements GatewayClientInterface
     protected $gatewayUrl;
 
     /**
-     * Gateway client user agent
+     * Curl options
      *
-     * @var string
+     * @var array
      */
-    protected $userAgent = 'PaynetEasy-Client/1.0';
+    protected $curlOptions = array
+    (
+        CURLOPT_HEADER         => 0,
+        CURLOPT_USERAGENT      => 'PaynetEasy-Client/1.0',
+        CURLOPT_SSL_VERIFYHOST => 0,
+        CURLOPT_SSL_VERIFYPEER => 0,
+        CURLOPT_POST           => 1,
+        CURLOPT_RETURNTRANSFER => 1
+    );
 
     /**
      * Paynet gateway client
      *
-     * @param       string      $gatewayUrl     Full url to Paynet API gateway
+     * @param       string      $gatewayUrl         Full url to Paynet API gateway
      */
     public function __construct($gatewayUrl)
     {
         $this->gatewayUrl = rtrim($gatewayUrl, '/');
+    }
+
+    /**
+     * Set curl options
+     *
+     * @param       array       $curlOptions        Curl options
+     *
+     * @return      self
+     */
+    public function setCurlOptions(array $curlOptions)
+    {
+        foreach ($curlOptions as $optionName => $optionValue)
+        {
+            $this->setCurlOption($optionName, $optionValue);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Set curl option
+     *
+     * @param       string      $optionName         Curl option name
+     * @param       string      $optionValue        Curl option value
+     *
+     * @return      self
+     */
+    public function setCurlOption($optionName, $optionValue)
+    {
+        $this->curlOptions[$optionName] = $optionValue;
+
+        return $this;
+    }
+
+    /**
+     * Delete all curl options
+     *
+     * @return      self
+     */
+    public function removeCurlOptions()
+    {
+        $this->curlOptions = array();
+
+        return $this;
     }
 
     /**
@@ -40,77 +94,84 @@ class GatewayClient implements GatewayClientInterface
 
         try
         {
-            $curl   = curl_init();
-            $url    = "{$this->gatewayUrl}/{$request->getApiMethod()}/{$request->getEndPoint()}";
-
-            /**
-             * @todo add method for change curl options
-             */
-            curl_setopt_array
-            (
-                $curl,
-                array
-                (
-                    CURLOPT_URL            => $url,
-                    CURLOPT_HEADER         => 0,
-                    CURLOPT_USERAGENT      => $this->userAgent,
-                    CURLOPT_SSL_VERIFYHOST => 0,
-                    CURLOPT_SSL_VERIFYPEER => 0,
-                    CURLOPT_POST           => 1,
-                    CURLOPT_RETURNTRANSFER => 1,
-                    CURLOPT_POSTFIELDS     => http_build_query($request->getRequestFields())
-                )
-            );
-
-            $response       = curl_exec($curl);
-
-            if(curl_errno($curl))
-            {
-                throw new RequestException(curl_error($curl), curl_errno($curl));
-            }
-
-            if(curl_getinfo($curl, CURLINFO_HTTP_CODE) != 200)
-            {
-                throw new RequestException
-                (
-                    sprintf
-                    (
-                    	'response http error code "%s"',
-                         curl_getinfo($curl, CURLINFO_HTTP_CODE)
-                    )
-                );
-            }
-
-            if(empty($response))
-            {
-                throw new RequestException('response is empty');
-            }
-
-            $result = array();
-
-            parse_str($response, $result);
-
-            if(empty($result))
-            {
-                throw new RequestException('Response parsing error: ' . $response);
-            }
-        }
-        catch (Exception $e)
-        {
-        }
+            $curl           = curl_init();
+            $responseString = $this->executeCurl($curl, $request);
+            $responseObject = $this->parseReponse($responseString);
+        }catch (Exception $e){}
         // finally
         {
-            // Close the resource
             curl_close($curl);
+            if(isset($e)) throw $e;
         }
 
-        // If exception has occured rethrow its
-        if(isset($e))
+        return $responseObject;
+    }
+
+    /**
+     * Executes request
+     *
+     * @param       resource        $curl           Curl instance
+     * @param       Request         $request        Request to execute
+     *
+     * @return      string                          Paynet response
+     *
+     * @throws      RequestException                Error while executing request
+     */
+    protected function executeCurl($curl, Request $request)
+    {
+        $url    = "{$this->gatewayUrl}/{$request->getApiMethod()}/{$request->getEndPoint()}";
+
+        curl_setopt_array($curl, array_merge
+        (
+            $this->curlOptions,
+            array
+            (
+                CURLOPT_URL         => $url,
+                CURLOPT_POSTFIELDS  => http_build_query($request->getRequestFields())
+            )
+        ));
+
+        $response = curl_exec($curl);
+
+        if(curl_errno($curl))
         {
-            throw $e;
+            throw new RequestException('Error occured: ' . curl_error($curl), curl_errno($curl));
         }
 
-        return new Response($result);
+        if(curl_getinfo($curl, CURLINFO_HTTP_CODE) != 200)
+        {
+            throw new RequestException('Error occured. HTTP code: ' . curl_getinfo($curl, CURLINFO_HTTP_CODE));
+        }
+
+        return $response;
+    }
+
+    /**
+     * Parse Paynet response from string to Response object
+     *
+     * @param       string      $response       Paynet response as string
+     *
+     * @return      Response                    Paynet response as object
+     *
+     * @throws      ResponseException           Error while parsing
+     */
+    protected function parseReponse($response)
+    {
+        if(empty($response))
+        {
+            throw new ResponseException('Paynet response is empty');
+        }
+
+        $responseFields = array();
+
+        parse_str($response, $responseFields);
+
+        if(empty($responseFields))
+        {
+            throw new ResponseException('Can not parse response: ' . $response);
+        }
+
+        return new Response($responseFields);
     }
 
     /**
