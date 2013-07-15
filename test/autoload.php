e494b2990ac5fe7bce5b939e1a946b1d0204f23d
imenem <imenem@inbox.ru>
2013-07-15 19:21:59 +0300

    Folder names are now singular.

--- test/autoload.php
+++ test/autoload.php
@@ -4,8 +4,8 @@ define('UNIT_TEST_ROOT', dirname(__FILE__));
 set_include_path
 (
     dirname(__FILE__).PATH_SEPARATOR.
-    dirname(__DIR__).'/tests/source/'.PATH_SEPARATOR.
-    dirname(__DIR__).'/tests'.PATH_SEPARATOR.
+    dirname(__DIR__).'/test/source/'.PATH_SEPARATOR.
+    dirname(__DIR__).'/test'.PATH_SEPARATOR.
     dirname(__DIR__).'/source'.PATH_SEPARATOR.
     get_include_path()
 );
